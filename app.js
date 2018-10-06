24fecca47e148710b82317f3b19b7f9907676970
Quentin Barroché <barrocheq@gmail.com>
2018-10-06 18:52:41 +0200

    link between front + back and add route for getAllCrypto

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var curl = require("curl")
 var fs = require("fs")
 var moment = require("moment")
+var cors = require('cors');
 const express = require('express')
 const app = express()
+
+
+app.use(cors())
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -13,8 +18,14 @@ app.get('/getChart/:crypto/:time', (req, res) => {
 	var time = req.params.time
 
 	getChart(crypto, time).then(chart => {
-		console.log(chart)
-		res.send(chart)
+		res.json(chart)
+	})
+})
+
+
+app.get('/getAllCrypto', (req, res) => {
+	getAllCrypto().then(cryptos => {
+		res.json(cryptos)
 	})
 })
 
@@ -23,20 +34,35 @@ app.listen(3000, function () {
 })
 
 
+function getAllCrypto() {
+	return new Promise((resolve, reject) => {
+		getAllPrice().then(crypto => {
+			var allCryp = []
+
+			crypto.forEach(row => {
+				allCryp.push(row.symbol)
+			})
+
+			resolve(allCryp)
+		})
+	})
+}
 
 function getAllPrice() {
-	curl.get('https://www.binance.com/api/v1/ticker/allPrices', (err, res, body) => {
-	    if(err)
-	        console.log(err)
+	return new Promise((resolve, reject) => {
+		curl.get('https://www.binance.com/api/v1/ticker/allPrices', (err, res, body) => {
+		    if(err)
+		        resolve(err)
 
-	    var myObj = []
+		    var myObj = []
 
-        JSON.parse(body).forEach(row => {
-            myObj.push(row)
-        })
+	        JSON.parse(body).forEach(row => {
+	            myObj.push(row)
+	        })
 
-        return myObj
-	})
+	        resolve(myObj)
+		})
+	})	
 }
 
 function getChart(symbol, interval) {
@@ -53,17 +79,17 @@ function getChart(symbol, interval) {
 			    body.forEach(row => {
 			    	let obj = {}
 
-			    	obj.openTime  			= row[0]
-			    	obj.openValue 			= row[1]
-			    	obj.high 				= row[2]
-			    	obj.low 				= row[3]
+			    	obj.openTime  			= moment(row[0]).format("DD/MM/YYYY")
+			    	// obj.openValue 			= row[1]
+			    	// obj.high 				= row[2]
+			    	// obj.low 				= row[3]
 			    	obj.closeValue 			= row[4]
-			    	obj.volume 				= row[5]
-			    	obj.closeTime 			= row[6]
-			    	obj.assetVolume 		= row[7]
-			    	obj.nbTrades 			= row[8]
-			    	obj.BaseAssetVolume 	= row[9]
-			    	obj.QuoteAssetVolume 	= row[10]
+			    	// obj.volume 				= row[5]
+			    	// obj.closeTime 			= row[6]
+			    	// obj.assetVolume 		= row[7]
+			    	// obj.nbTrades 			= row[8]
+			    	// obj.BaseAssetVolume 	= row[9]
+			    	// obj.QuoteAssetVolume 	= row[10]
 			    	// obj.ignore 				= row[11] // USELESS VALUE
 
 			    	myObj.push(obj)
